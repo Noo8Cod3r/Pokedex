ef78059033b3bbfd0805e83258a0f01eafed4d4f
Noo8 Cod3r <noo8cod3r@noo8s-mbp.attlocal.net>
2019-04-25 16:17:24 -0500

    Pokecard Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import Pokecard from '../src/Components/Pokecard'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Pokedex</h1>
       </header>
+      <Pokecard 
+        id = {4}
+        name = 'Charmander'
+        type = 'fire'
+        exp = {62}
+      />
     </div>
   );
 }
---
+++ src/Components/Pokecard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+const POKI_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+export default class Pokecard extends Component {
+    render() {
+        let imgSrc = `${POKI_API}${this.props.id}.png`
+        return(
+          <div className = 'Pokecard'>
+            <h1>{this.props.name}</h1>
+            <img src={imgSrc} alt={this.props.name} />
+            <div>Type: {this.props.type}</div>
+            <div>Experiance: {this.props.exp} </div>
+          </div>
+        )
+    }
+}
