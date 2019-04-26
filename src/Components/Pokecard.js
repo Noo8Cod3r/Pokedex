390622fd4b7ab55adad58712bb657bc6c1c2e48d
Noo8 Cod3r <noo8cod3r@noo8s-mbp.attlocal.net>
2019-04-25 22:44:48 -0500

    Update

--- src/Components/Pokecard.js
+++ src/Components/Pokecard.js
@@ -3,17 +3,19 @@ import './Pokecard.css';
 
 
 
-const POKI_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKI_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 export default class Pokecard extends Component {
     render() {
-        let imgSrc = `${POKI_API}${this.props.id}.png`
+        let imgSrc = `${POKI_API}${padToThree(this.props.id)}.png`
         return(
           <div className = 'Pokecard'>
-            <h1>{this.props.name}</h1>
+            <h1 className = 'Pokecard-title'>{this.props.name}</h1>
             <img src={imgSrc} alt={this.props.name} />
-            <div>Type: {this.props.type}</div>
-            <div>Experiance: {this.props.exp} </div>
+            <div className = 'Pokecard-data'>Type: {this.props.type}</div>
+            <div className = 'Pokecard-data'>Experiance: {this.props.exp} </div>
           </div>
         )
     }
