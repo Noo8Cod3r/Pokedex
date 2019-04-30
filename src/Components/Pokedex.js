d0ea2317bafc24b4df6b2d09f39e55770f43ca96
Noo8 Cod3r <noo8cod3r@noo8s-mbp.attlocal.net>
2019-04-29 22:39:11 -0500

    Game Logic

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -5,19 +5,6 @@ import'./Pokedex.css'
 
 
 export default class Pokedex extends Component {
-    static defaultProps = {
-        pokemon : [
-            { id: 34, name: 'Nidoking', type: 'Poison', exp: 505 },
-            { id: 9, name: 'Blastoise', type: 'Water', exp: 530 },
-            { id: 6, name: 'Charizard', type: 'Fire', exp: 534 },
-            { id: 15, name: 'Beedrill', type: 'Bug', exp: 395 },
-            { id: 26, name: 'Raichu', type: 'Electric', exp: 485 },
-            { id: 53, name: 'Persian', type: 'Normal', exp: 440 },
-            { id: 150, name: 'Mewtwo', type: 'Psychic', exp: 680 },
-            { id: 136, name: 'Flareon', type: 'Fire', exp: 525 }
-        ]
-    };
-
     render() {
       let title;
       if(this.props.isWinner){
@@ -36,7 +23,7 @@ export default class Pokedex extends Component {
                 id={p.id}
                 name={p.name}
                 type={p.type}
-                exp={p.exp}
+                exp={p.base_exp}
               />
             ))}
             </div>
